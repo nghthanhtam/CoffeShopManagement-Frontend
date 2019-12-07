f2796ec3b8edd068e86af37df0413ad55121a759
Julia Hạ Vy <16521470@gm.uit.edu.vn>
2019-12-07 20:59:17 +0700

    add redux dev tool and redux-logger

--- src/store.js
+++ src/store.js
@@ -1,9 +1,17 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import rootReduce from './reducers'
 
 const middleware = [thunk]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(logger)
+}
 
-const store = createStore(rootReduce, applyMiddleware(...middleware))
+const store = createStore(
+  rootReduce,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
 
 export default store
